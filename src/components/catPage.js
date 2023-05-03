2f6559df9f9baa53021c8a20696b909db90d7fdf
nicknicknick4444 <94404473+nicknicknick4444@users.noreply.github.com>
2023-05-03 18:49:05 +0100

    Add files via upload

--- src/components/catPage.js
+++ src/components/catPage.js
@@ -7,7 +7,7 @@ import {Navbar} from "./navBar.js";
 import {SearchBox} from "./searchBox.js";
 import {Header} from "./header.js";
 import {useProps} from "./hooks/prop-hooks.js";
-import {toTitle} from "./hooks/hooks.js";
+import {getty, setty, toTitle} from "./hooks/hooks.js";
 import {ErrorBoundary} from "./errorBoundary.js";
 
 export function CatPage() {
@@ -15,19 +15,22 @@ export function CatPage() {
     const [allsubcats, setAllsubcats] = useState([]);
     const [subcat, setSubcat] = useState([]);
     const [catto, setCatto] = useState("");
-    const {category, setCategory, setSearched, section, setChecking} = useProps();
+    const {category, setCategory, setSearched, section, 
+        setChecking, prods_api} = useProps();
+
     const getSubcats = () => {
         axios
-            .get(`https://polar-coast-39563.herokuapp.com/api/subcat/`)
+            .get(`http://localhost:8000/api/subcat/`)
+            // .get(`https://polar-coast-39563.herokuapp.com/api/subcat/`)
             .then((res) => setAllsubcats(res.data))
             .catch(err =>console.log("Error: ", err));
     };
 
     useEffect(() => {
         getSubcats();
+        console.log("Ran the real API!");
         setSearched(false);
         setChecking(true);
-        // setCategory(cat_name);
     }, []);
 
     useEffect(() => {
@@ -68,11 +71,14 @@ export function CatPage() {
             <div>
                 {
                     subcat.map((subby, key) => (
+                        getty("all_cats") !== null && getty("all_cats").length > 0 && getty("all_cats").indexOf(subby["subcat_name"]) !== -1 ? 
                             <div key={key} >
                                 <Link to={{pathname: `${subby.subcat_name}`}}>{subby.subcat_name}<br />
                                 <img src={`${subby.subcat_image}`} style={{ width: 200 }} />
                                 </Link>
                             </div>
+                            :
+                            <span key={key+"a"}>Beancie</span>
                     ))
                 }
             </div>
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,23 +1,30 @@
 import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {useProps} from "./hooks/prop-hooks.js";
-import {toTitle} from "./hooks/hooks.js";
+import {setty, getty, toTitle} from "./hooks/hooks.js";
 import {BasketPage} from "./basketList.js";
 import axios from "axios";
 import "../App.css";
 
 export function Navbar() {
     const [cats, setCats] = useState([]);
+    const {allcats, prods_api} = useProps();
+    var cats2 = [];
 
     const getCats = () => {
         axios
-            .get(`https://polar-coast-39563.herokuapp.com/api/category/`)
+            .get(`http://localhost:8000/api/category/`)
+            // .get(`https://polar-coast-39563.herokuapp.com/api/category/`)
             .then((res) => setCats(res.data))
             .catch(err => console.log("Error: ", err));
     };
 
     useEffect(() => {
         getCats();
+        if (getty("all_cats") === null || getty("all_cats") === 0) {
+            prods_api();
+        };
+        console.log("Blues! ", allcats);
     }, []);
 
     useEffect(() => {
@@ -33,12 +40,16 @@ export function Navbar() {
             <span className="navbar"><Link to={{pathname: "/"}}>Home</Link></span>
             {   
                 cats.map((cat, key) => (
+                    getty("all_cats") !== null && getty("all_cats").length > 0 && getty("all_cats").indexOf(cat["cat_name"]) !== -1 ? 
                     <span key={key} className="navbar">
                         <Link to={{pathname: `../${cat.cat_name}`}}>
                             {toTitle(cat.cat_name)}
                         </Link>
                     </span>
+                    :
+                    <span key={key+"b"}>Beance {cat["cat_name"]}</span>
                 ))
+            
             }
             </div><br /><br /><br />
         </>
