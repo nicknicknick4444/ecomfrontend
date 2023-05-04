ce88281449da4f1242fad75fbaab58566417d2c4
nicknicknick4444 <94404473+nicknicknick4444@users.noreply.github.com>
2023-05-04 23:05:14 +0100

    Add files via upload

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -54,6 +54,8 @@ export function Navbar() {
             </div>
             <div className="navbar-mob">
             <div onClick={() => {setBurger(!burger)}}>BURGER! {burger.toString()}</div>
+                {burger ? 
+                <div>
                 <span><Link to={{pathname: "/"}}>Home</Link></span><br />
                 {
                     cats.map((cat, key) => (
@@ -68,6 +70,7 @@ export function Navbar() {
                         <span className="navbar-mob" key={key+"b-mob"}>Beance mobile! {toTitle(cat["cat_name"])}!</span>
                     ))
                 }
+                </div>: null}
             </div>
             <br /><br /><br />
         </>
