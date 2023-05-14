6e0900a35adfc65e6c4f803ba97dddfdd18e9e26
nicknicknick4444 <94404473+nicknicknick4444@users.noreply.github.com>
2023-05-14 19:36:40 +0100

    Add files via upload

--- src/components/basket.js
+++ src/components/basket.js
@@ -14,6 +14,7 @@ export function Basket() {
         if (totally) {
             setAmount(begin());
         };
+        console.log("AMOOOONT! ", length_pixels());
     }, [totally]);
 
     useEffect(() => {
@@ -23,19 +24,37 @@ export function Basket() {
         console.log("WHACKO! ", basko);
     }, [droplist]);
 
+    function length_pixels() {
+        var extra = 0;
+        if (amount <= 99.99) {
+            extra = 0;
+        } else if (amount >= 100.00 && amount <= 999.99) {
+            extra = 10;
+        } else if (amount >= 1000.00 && amount <= 10000.00) {
+            extra = 20;
+        } else if (amount >= 10000.00 && amount <= 99999.99) {
+            extra = 25;
+        } else if (amount >= 100000.01) {
+            extra = 30;
+        }
+        return extra;
+    };
+
     // if (localStorage.getItem("new") !== null) {
     if (getty("new") !== null) {
         return (
             <>
                 <ErrorBoundary>
-                    <div className="basket-pc" onMouseOver={() => {setDroplist(true); get_location()}} onMouseLeave={() => setDroplist(false)}>
+                    <div className="basket-pc" onMouseOver={() => {setDroplist(true); get_location()}} onMouseLeave={() => setDroplist(false)} 
+                        style={{width: 120 + length_pixels()}}>
+                    <Link to="/basket-page">
                     <div><img src="https://i.ibb.co/G31Vcrk/basket.png" className="basket-icon" /></div>
                     <div className="basket-words">
                         <div>Items: <span clasName="basket_total">{!bought ? totally < 0 ? "0.00" : totally : 0 }</span></div>
                         <div>Total: &nbsp;<span className="price-pad">£{!bought ? parseInt(amount) == 0 ? "0.00" : amount.toFixed(2) : "0.00"}</span></div> 
     
                         {!bought ?
-                        <div id="view-basket" onClick={() => setChecking(false)}><Link to="/basket-page"><i>View Basket</i></Link></div>
+                        <div id="view-basket" onClick={() => setChecking(false)}><i>View Basket</i></div>
                         :
                         <div id="view-basket" onClick={() => {setChecking(false); empty(setDis, updateTotal, setFillingdeets)}}>
                             <Link to="/basket-page"><i>View Basket</i></Link>
@@ -49,7 +68,7 @@ export function Basket() {
                     </div>
                     : null
                     }
-
+                    </Link>
                     </div>
                     {/* {droplist ? 
                     <div className="dropdown-basket">
