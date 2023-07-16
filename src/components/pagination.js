10b4623aede20613a016d1bcac3ddba3d5f57b6f
nicknicknick4444 <94404473+nicknicknick4444@users.noreply.github.com>
2023-07-16 10:25:14 +0100

    Add files via upload

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -14,6 +14,7 @@ export function ProductPagination() {
         window.scrollTo(0, 0);
         // setPage([5, 9]);
         setPage([clicked, limit]);
+        console.log("Subset? DUBSET!");
         // console.log("SUBSET!", subset);
     }, [page[0]]);
 
