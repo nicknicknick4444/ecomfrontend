27cb208bfdab859ebc71f02c7f4e18b2efc96c39
nicknicknick4444 <94404473+nicknicknick4444@users.noreply.github.com>
2023-07-29 11:58:20 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-//import logo from './logo.svg';
-import React, {useEffect, Component} from "react";
-// import ModalY from "./components/Modal.js";
-// import axios from "axios";
-import {Link, useLocation, useRoutes} from "react-router-dom";
+import React, {useEffect} from "react";
+import {Link, useRoutes} from "react-router-dom";
 import './App.css';
-import {Navbar} from "./components/navBar.js";
-import {SearchBox} from "./components/searchBox.js";
 import {Header} from "./components/header.js";
 import {Footer} from "./components/footer.js";
 import {ProdPage} from "./components/prodPage.js";
 import {CatPage} from "./components/catPage.js";
 import {SubcatPage} from "./components/subcatPage.js";
-import {Basket} from "./components/basket.js";
 import {ShowBasket} from "./components/showBasket.js";
 import {Checkout} from "./components/checkout.js";
 import {SearchPage} from "./components/searchPage.js";
 import {General} from "./components/general.js";
 import {useProps} from "./components/hooks/prop-hooks.js";
-import {getty, setty, toTitle} from "./components/hooks/hooks.js";
-import {addToBasket, removeFromBasket} from "./components/basketButtons.js";
-import {basketUpdate, AllInput} from "./components/basketUpdate.js";
+
 
 function AppFront() {
   const {setSearched, setChecking, setBurger, 
@@ -86,7 +78,13 @@ function App2() {
     {path: "checkout", element: <Checkout />},
     {path: "search-results", element: <SearchPage />},
     {path: "about-us", element: <General />},
-    {path: "contact-us", element: <General />}
+    {path: "contact-us", element: <General />},
+    {path: "FAQ", element: <General />},
+    {path: "picks-of-the-month", element: <General />},
+    {path: "deliveries-and-Returns", element: <General />},
+    {path: "careers", element: <General />},
+    {path: "privacy-policy", element: <General />},
+    {path: "terms-and-conditions", element: <General />}
   ]);
   return element;
 };
