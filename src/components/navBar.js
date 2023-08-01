d600969b65719c43896d53be4242a3505179ff7c
nicknicknick4444 <94404473+nicknicknick4444@users.noreply.github.com>
2023-08-01 10:36:58 +0100

    Add files via upload

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -90,7 +90,7 @@ export function Navbar() {
                             <Link to={{pathname: "/Appliances"}}>Appliances</Link>
                         </span>
                         <span className="navbar">
-                            <Link to={{pathname: "/Cleaning%20&%20Hygiene"}}>Cleaning & Hygiene</Link>
+                            <Link to={{pathname: "/Cleaning%20&%20Hygiene"}}>Cleaning</Link>
                         </span>
                     </div>
 
@@ -109,7 +109,7 @@ export function Navbar() {
                         <span className="navbar-mob"><Link to={{pathname: "/Kitchenware"}}>Kitchenware</Link></span><br />
                         <span className="navbar-mob"><Link to={{pathname: "/Furniture"}}>Furniture</Link></span><br />
                         <span className="navbar-mob"><Link to={{pathname: "/Appliances"}}>Appliances</Link></span><br />
-                        <span className="navbar-mob"><Link to={{pathname: "/Cleaning%20&%20Hygiene"}}>Cleaning & Hygiene</Link></span><br />
+                        <span className="navbar-mob"><Link to={{pathname: "/Cleaning"}}>Cleaning</Link></span><br />
                         <span className="navbar-mob"><Link to={{pathname: "/about-us"}}>About Us</Link></span><br />
                         <span className="navbar-mob"><Link to={{pathname: "/contact-us"}}>Contact Us</Link></span>
                     </div> : null}
